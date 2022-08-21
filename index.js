707fb631605a6653b1c0f75b795efe489f2d50a2
Romane Orlando Robb <mail@romanerobb.com>
2022-08-21 10:45:38 -0400

    updates

---
+++ index.js
@@ -0,0 +1,27 @@
+let softwareDeveloper = 'Romane Orlando Robb';
+console.log(softwareDeveloper);
+
+var myName = "Romane Orlando Robb";
+alert(myName);
+
+//Switch variable from a to b.
+
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+
+var c = a
+var a = b;
+var b = c;
+
+    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+console.log(test);
